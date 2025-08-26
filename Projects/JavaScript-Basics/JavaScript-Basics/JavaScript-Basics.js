d9e61ffa9a98e5d726b4de13991020db2d7df1a3
Rohric <emilmarsal@icloud.com>
2025-08-26 22:16:19 +0200

    new order

---
+++ Projects/JavaScript-Basics/JavaScript-Basics/JavaScript-Basics.js
@@ -0,0 +1,8 @@
+
+// Training 2 Farbwechsler
+
+let colorBox = document.getElementById('btn_color');
+
+colorBox.addEventListener('click', () => {
+  colorBox.classList.toggle('leuchtendes_Cyan');
+});
