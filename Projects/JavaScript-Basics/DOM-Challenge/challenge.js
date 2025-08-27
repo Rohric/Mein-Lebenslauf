55be4c74ddfa3d5d83c7606ee99cafe0b58ae0e8
Rohric <emilmarsal@icloud.com>
2025-08-27 10:07:53 +0200

    add debugger challenge

--- Projects/JavaScript-Basics/DOM-Challenge/challenge.js
+++ Projects/JavaScript-Basics/DOM-Challenge/challenge.js
@@ -1,14 +1,12 @@
-//nim eingabe vom inputfeld
-//wenn man auf senden drückt
-//wird kommentar in der div angezeigt
-
+//Aufgabe 1
 
 function returnComment(){
 
     let comment = document.getElementById("comment").value;
-    document.getElementById('comment_area').innerText += comment;
-
+    document.getElementById('comment_area').innerHTML += "<p>"+ comment +"</p>";
 }
+
+// Aughabe 2
 
 let counterField = document.getElementById('counter_field').innerText.value;
 console.log(counterField.length);
