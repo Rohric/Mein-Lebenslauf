3ca9c641278c4ec55d9e6c086cac08d1714fd7b1
Rohric <emilmarsal@icloud.com>
2025-08-31 17:36:53 +0200

    finish notizblock ohne local stotage

--- Sites&Projects/Modul-7-Sektion-1-Notizblock/script.js
+++ Sites&Projects/Modul-7-Sektion-1-Notizblock/script.js
@@ -1,8 +1,67 @@
-// notizen anzeigen lassen. 
-// ich brauche notizen
-// ich muss defienieren wo sie anzuzeigen sind
+//
+//
 
+let notesTitles = ['Fruit', 'Aufgabe']
+let notes = ["banana", "rasen mähen"];
+let trashNotesTitle = [];
+let trashNotes = [];
 
-// Notizen hinzufügen
-//Notizen löschen
-// Notizen archivieren
+// Notizen anzeigen lassen / rendern
+function renderNotes() {
+  let contentRef = document.getElementById("content");
+  contentRef.innerHTML = "";
+
+  for (let indexNote = 0; indexNote < notes.length; indexNote++) {
+    contentRef.innerHTML += "" + getNoteTemplate(indexNote);
+  }
+}
+// Trash anzeigen lassen / rendern
+function renderTrashNotes() {
+  let trashContentRef = document.getElementById("trash_content");
+  trashContentRef.innerHTML = "";
+
+  for (
+    let indexTrashNote = 0;
+    indexTrashNote < trashNotes.length;
+    indexTrashNote++
+  ) {
+    trashContentRef.innerHTML += "" + getTrashNoteTemplate(indexTrashNote);
+  }
+}
+
+// neue Notiz generieren Template erstellen
+function getNoteTemplate(indexNote) {
+  return `<p>+ title: ${notesTitles[indexNote]} -> ${notes[indexNote]}<button onclick="moveToTrash(${indexNote})">x</button></p>`;
+}
+function getTrashNoteTemplate(indexTrashNote) {
+  return `<p>+ title: ${trashNotesTitle[indexTrashNote]} ->  ${trashNotes[indexTrashNote]}<button onclick="deleteNote(${indexTrashNote})">x</button></p>`;
+}
+
+// notizen hinzufügen
+function addNote() {
+  let noteInputRef = document.getElementById("note_input"); // eigabe auslesen
+  let noteInput = noteInputRef.value;
+
+  notes.push(noteInput); // eingabe speichern ,den notizen hinzufügen, heißt wir müssen es dem note Array hinzufügen
+
+  renderNotes(); // eingabe anzeigen lasse
+  noteInputRef.value = " ";
+}
+
+//Papierkorb
+//Wann muss die notiz gelöscht werden
+function moveToTrash(indexNote) {
+  let trashNote = notes.splice(indexNote, 1); //welche notiz muss gelöscht werden
+  trashNotes.push(trashNote);
+  let trashNotesTitles = notesTitles.splice(indexNote, 1); //welche title muss gelöscht werden
+  trashNotesTitle.push(trashNotesTitles);
+  renderNotes(); // anzeige updaten
+  renderTrashNotes(trashNotes); // TrashNotes anzeige lassen
+}
+
+//notiz entgültig löschen
+function deleteNote() {
+  trashNotes.splice(trashNotes, 1); // notiz raus splicen also löschen
+  renderNotes(); // anzeige updaten
+  renderTrashNotes(trashNotes); // TrashNotes anzeige lassen
+}
