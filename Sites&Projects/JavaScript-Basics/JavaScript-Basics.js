4592fcedef781c908d7080543f78fd295072a6f8
Rohric <emilmarsal@icloud.com>
2025-08-31 19:57:18 +0200

    Revert "eigenbau der index"
    This reverts commit 365b5520dcdf520a2d75750aafa1ca6efaba6534.

--- Sites&Projects/JavaScript-Basics/JavaScript-Basics.js
+++
@@ -1,8 +0,0 @@
-
-// Training 2 Farbwechsler
-
-let colorBox = document.getElementById('btn_color');
-
-colorBox.addEventListener('click', () => {
-  colorBox.classList.toggle('leuchtendes_Cyan');
-});
